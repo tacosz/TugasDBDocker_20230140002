17229427c82785c8d1effe365ef4fd8e78159e50
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-04-21 14:04:05 +0700

    import dependencies

---
+++ src/main/java/com/praktikumDB/deploy/model/User.java
@@ -0,0 +1,17 @@
+package com.praktikumDB.deploy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table (name = "20230140002")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+}
